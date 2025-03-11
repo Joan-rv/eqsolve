7a8eb33efbe429106ae12f2f2e286e582256041f
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-03-11 13:13:11 +0100

    Rename variable

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn secant(f: fn(f64) -> f64, mut x0: f64, mut x1: f64, error: f64) -> f64 {
     while f(x1).abs() > error || (x1 - x0).abs() > error {
-        let x2 = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0));
+        let x_new = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0));
         x0 = x1;
-        x1 = x2;
+        x1 = x_new;
     }
     x1
 }
