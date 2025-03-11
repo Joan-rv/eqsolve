6b639b1179cc93b67fb2543c8f3f9f6d03949795
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-03-11 13:19:14 +0100

    Bisection method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn bisection(f: fn(f64) -> f64, mut b: f64, mut a: f64, error: f64) -> f64 {
+    let mut x = (a + b) / 2.0;
+    while a - b > error {
+        if f(x) == 0.0 {
+            break;
+        }
+        if f(a)*f(x) < 0.0 {
+            b = x;
+        } else {
+            a = x;
+        }
+        x = (a + b) / 2.0;
+    }
+    x
+}
+
 fn secant(f: fn(f64) -> f64, mut x0: f64, mut x1: f64, error: f64) -> f64 {
     while f(x1).abs() > error || (x1 - x0).abs() > error {
         let x_new = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0));
@@ -29,4 +45,5 @@ fn main() {
     let error = 0.00001;
     println!("Solution with newton: {}", newton(f, 2.0, error));
     println!("Solution with secant: {}", secant(f, 1.0, 3.0, error));
+    println!("Solution with bisection: {}", bisection(f, 1.0, 3.0, error));
 }
