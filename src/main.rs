1e431ef1b3bca33661fd5125a037973fecedf213
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-03-11 13:11:03 +0100

    Secant method implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn secant(f: fn(f64) -> f64, mut x0: f64, mut x1: f64, error: f64) -> f64 {
+    while f(x1).abs() > error || (x1 - x0).abs() > error {
+        let x2 = (x0 * f(x1) - x1 * f(x0)) / (f(x1) - f(x0));
+        x0 = x1;
+        x1 = x2;
+    }
+    x1
+}
+
 fn differentiate(f: fn(f64) -> f64, x: f64) -> f64 {
     let e: f64 = 2.2e-16;
     let h = e.sqrt();
@@ -17,5 +26,7 @@ fn newton(f: fn(f64) -> f64, mut x: f64, error: f64) -> f64 {
 
 fn main() {
     let f = |x: f64| {x*x - 2.0};
-    println!("Solution: {}", newton(f, 2.0, 0.00001));
+    let error = 0.00001;
+    println!("Solution with newton: {}", newton(f, 2.0, error));
+    println!("Solution with secant: {}", secant(f, 1.0, 3.0, error));
 }
